61b0c7ff7a5c4741136027910f8e9e893355bea5
Rutul Shah <Rutul.Shah@mrisoftware.com>
2026-04-22 08:46:15 +0530

    api changes for create/update employee form

--- src/api/employees.js
+++ src/api/employees.js
@@ -4,16 +4,16 @@ export const getEmployees = (params= {}) =>
     client.get('employees',{ params }).then(res => res.data)
 
 export const getEmployee = (id) =>
-    client.get('employees/${id}').then(res => res.data)
+    client.get(`employees/${id}`).then(res => res.data)
 
 export const createEmployee = (employee) =>
-    client.get('employees', employee).then(res => res.data)
+    client.post('employees', { employee }).then(res => res.data)
 
 export const updateEmployee = (id, employee) =>
-    client.patch('/employees/${id}', { employee }).then(res => res.data);
+    client.patch(`/employees/${id}`, { employee }).then(res => res.data);
 
 export const deleteEmployee = (id) =>
-    client.delete('/employees/${id}');
+    client.delete(`/employees/${id}`);
 
 export const getSalaryInsights = (params = {}) =>
     client.get('/salary_insights', { params }).then(res => res.data);
